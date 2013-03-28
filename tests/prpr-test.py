101f28add5391860ac5fd653f29fbd3e8982828d
Nina Stawski <me@ninastawski.com>
2013-03-28 15:53:13 -0700

    Added basic test

--- tests/prpr-test.py
+++ tests/prpr-test.py
@@ -1,5 +1,9 @@
 import unittest
 
+import os,sys
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,parentdir)
+
 import prpr
 
 class TestServer(unittest.TestCase):
